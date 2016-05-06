11bf8704bfb56f5f397ee2a1d8c90a7f5a9329e6
acerion <acerion@wp.pl>
2016-05-06 19:15:10 +0200

    New files for code moved from viktrwlayer.cpp/h

---
+++ src/layer_trw_containers.h
@@ -0,0 +1,31 @@
+/*
+ * SlavGPS -- GPS Data and Topo Analyzer, Explorer, and Manager
+ *
+ * A fork of viking, project started in 2016.
+ *
+ * Copyright (C) 2003-2005, Evan Battaglia <gtoevan@gmx.net>
+ * Copyright (c) 2011-2013, Rob Norris <rw_norris@hotmail.com>
+ * Copyright (c) 2016 Kamil Ignacak <acerion@wp.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _SG_LAYER_TRW_CONTAINERS_H
+#define _SG_LAYER_TRW_CONTAINERS_H
+
+
+
+#endif /* #ifndef _SG_LAYER_TRW_CONTAINERS_H */
