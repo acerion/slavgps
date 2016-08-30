032ed2a79c023cc54ac4c520b5faea3b738a800d
acerion <acerion@wp.pl>
2016-08-30 18:17:34 +0200

    New, temporary files with some mock definitions and declarations.

---
+++ src/slav_qt.cpp
@@ -0,0 +1,92 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define DEFAULT_BACKGROUND_COLOR "#CCCCCC"
+#define DEFAULT_HIGHLIGHT_COLOR "#EEA500"
+/* Default highlight in orange */
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#include <cstdlib>
+#include <cassert>
+
+#ifndef SLAVGPS_QT
+#include <glib-object.h>
+#endif
+
+#include "vikviewport.h"
+#include "coords.h"
+#ifndef SLAVGPS_QT
+#include "window.h"
+#endif
+#include "mapcoord.h"
+
+/* For ALTI_TO_MPP. */
+#include "globals.h"
+#include "settings.h"
+#ifndef SLAVGPS_QT
+#include "dialog.h"
+#endif
+
+
+
+
+/* Mock functions used during migration of SlavGPS to QT. */
+
+using namespace SlavGPS;
+
+
+
+
+#ifdef SLAVGPS_QT
+
+
+DistanceUnit a_vik_get_units_distance(void)
+{
+	return (DistanceUnit) 0;
+}
+
+vik_startup_method_t a_vik_get_startup_method()
+{
+	return VIK_STARTUP_METHOD_LAST_LOCATION;
+}
+
+void a_settings_set_double(char const*, double)
+{
+
+}
+
+bool a_settings_get_double(char const*, double*)
+{
+	return true;
+}
+
+bool a_settings_get_integer(char const*, int*)
+{
+	return true;
+}
+
+double a_vik_get_default_lat()
+{
+	return 55.0;
+}
+
+double a_vik_get_default_long()
+{
+	return 16.0;
+}
+
+vik_degree_format_t a_vik_get_degree_format()
+{
+	return VIK_DEGREE_FORMAT_DDD;
+}
+
+
+
+#endif
---
+++ src/slav_qt.h
@@ -0,0 +1,24 @@
+#ifndef SLAV_QT_H
+#define SLAV_QT_H
+
+
+/* Mock definitions used during migration of SlavGPS to QT. */
+
+
+
+#ifdef SLAVGPS_QT
+typedef int GdkGC;
+typedef int GdkFont;
+typedef int GdkColor;
+typedef int GtkWindow;
+typedef int GtkWidget;
+typedef void GdkPixbuf;
+typedef void Layer;
+typedef void GdkFunction;
+typedef int PangoLayout;
+typedef int GdkPixmap;
+typedef int GtkDrawingArea;
+#endif
+
+
+#endif
