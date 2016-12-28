7a3b052d6a320adece489b0d7c2590c96b11c19b
acerion <acerion@wp.pl>
2016-12-28 18:50:58 +0100

    Moving some code from window_layer_tools.* to layer_toolbox.*

--- src/layer_toolbox.h
+++ src/layer_toolbox.h
@@ -4,7 +4,66 @@
 
 
 
+#include <vector>
+
+#include <QAction>
+#include <QMouseEvent>
+#include <QCursor>
+
+#include "layer.h"
+
+
+
+
 namespace SlavGPS {
+
+
+
+
+	class Window;
+
+
+
+
+	class LayerToolbox {
+
+	public:
+		LayerToolbox(Window * win) : window(win) {};
+		~LayerToolbox();
+
+		QAction * add_tool(LayerTool * layer_tool);
+		void add_group(QActionGroup * group);
+		LayerTool * get_tool(QString const & tool_name);;
+
+		void activate_tool(QAction * qa);
+		bool deactivate_tool(QAction * qa);
+
+		void selected_layer(QString const & group_name);
+
+		QAction * set_group_enabled(QString const & group_name);
+		QActionGroup * get_group(QString const & group_name);
+		QAction * get_active_tool_action(void);
+		LayerTool * get_active_tool(void);
+
+		QCursor const * get_cursor_click(QString const & tool_name);
+		QCursor const * get_cursor_release(QString const & tool_name);
+
+		void click(QMouseEvent * event);
+		void double_click(QMouseEvent * event);
+		void move(QMouseEvent * event);
+		void release(QMouseEvent * event);
+
+
+
+		LayerTool * active_tool = NULL;
+		QAction * active_tool_qa = NULL;
+		unsigned int n_tools = 0;
+		std::vector<LayerTool *> tools;
+		Window * window = NULL;
+
+	private:
+		std::vector<QActionGroup *> action_groups;
+	};
 
 
 
