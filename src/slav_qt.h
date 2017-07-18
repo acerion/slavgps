00326cb03ca5a96a7002ae85b63485f963f79c22
acerion <acerion@wp.pl>
2017-07-18 21:51:36 +0200

    Some typedefs were unused and have been removed. Other have been moved to places that still need them.

--- src/slav_qt.h
+++ src/slav_qt.h
@@ -10,37 +10,7 @@
 
 
 #ifdef SLAVGPS_QT
-typedef int GtkListStore;
-typedef int GtkUIManager;
-typedef int GtkActionGroup;
-typedef int GdkEventExpose;
-typedef int GValue;
-typedef int GType;
-typedef int GtkRange;
-typedef int GParamSpec;
-typedef int GtkToggleButton;
 typedef int GtkWidget;
-typedef int GdkFunction;
-typedef int PangoLayout;
-typedef int GtkDrawingArea;
-typedef void GObject;
-typedef int ui_change_values;
-typedef int GtkTreeIter;
-typedef int GdkEventButton;
-typedef int GdkEventMotion;
-typedef int GtkTreePath;
-typedef struct { int*a; int*b; int*c; int*d; int*e; int f; } GtkRadioActionEntry;
-typedef int GdkPixdata;
-typedef int GtkTreeDragSource;
-typedef int GtkTreeDragDest;
-typedef int GtkSelectionData;
-typedef int GtkCellRenderer;
-typedef int GtkCellRendererToggle;
-typedef int GtkCellRendererText;
-typedef int GtkCellEditable;
-typedef int GtkTreeSelection;
-typedef int GtkTreeStore;
-typedef int GtkTreeViewColumn;
 #endif
 
 
