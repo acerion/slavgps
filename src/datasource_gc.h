02c171eeb5df07f03da58d20efa99cda76c8cc10
acerion <acerion@wp.pl>
2017-11-19 13:49:15 +0100

    Adding to repository missing file
    I have created it not so long ago.

---
+++ src/datasource_gc.h
@@ -0,0 +1,81 @@
+/*
+ * viking -- GPS Data and Topo Analyzer, Explorer, and Manager
+ *
+ * Copyright (C) 2003-2007, Evan Battaglia <gtoevan@gmx.net>
+ * Copyright (C) 2015, Rob Norris <rw_norris@hotmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#ifndef _SG_DATASOURCE_GC_H_
+#define _SG_DATASOURCE_GC_H_
+
+
+
+
+#include <QSpinBox>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+
+
+
+
+#include "datasource.h"
+
+
+
+
+namespace SlavGPS {
+
+
+
+
+	class Viewport;
+
+
+
+
+	class DataSourceGCDialog : public DataSourceDialog {
+		Q_OBJECT
+	public:
+		DataSourceGCDialog();
+		~DataSourceGCDialog();
+
+		ProcessOptions * get_process_options(DownloadOptions & dl_options);
+
+		QSpinBox num_spin;
+		QLineEdit center_entry; /* TODO make separate widgets for lat/lon. */
+		QDoubleSpinBox miles_radius_spin;
+
+		QPen circle_pen;
+		Viewport * viewport = NULL;
+		bool circle_onscreen;
+		int circle_x;
+		int circle_y;
+		int circle_width;
+
+	private slots:
+		void draw_circle_cb(void);
+
+	};
+
+
+
+
+} /* namespace SlavGPS */
+
+
+
+
+#endif /* #ifndef _SG_DATASOURCE_GC_H_ */
